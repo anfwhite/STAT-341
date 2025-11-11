0e631f5acb938c764560efbfe8c0d7867091c8cf
anfwhite <111890870+anfwhite@users.noreply.github.com>
2025-11-11 15:53:34 -0500

    Add files via upload

---
+++ chi-square-test.R
@@ -0,0 +1,15 @@
+# Author: White; Date: 11/11/25; Purpose: Test the chi-square analysis
+
+# Load dummy dataset
+data_frame <- read.csv("https://goo.gl/j6lRXD")
+
+table(data_frame$treatment, data_frame$improvement)
+
+# Apply chi-figure function to check the difference between treatment and improvement 
+
+chisq.test(data_frame$treatment, data_frame$improvement, correct=FALSE)
+
+  Pearson's Chi-squared test
+
+data:  data_frame$treatment and data_frame$improvement
+X-squared = 5.5569, df = 1, p-value = 0.01841
